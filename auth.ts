ca7da534e34798e2347f98cf7b5655c0376e5d5c
fajrijuanda <if22.fajrijuanda@mhs.ubpkarawang.ac.id>
2026-03-08 13:07:15 +0700

    feat: Implement Credentials provider for email/password authentication with Zod validation and bcrypt.

--- auth.ts
+++ auth.ts
@@ -1,10 +1,43 @@
 import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
+import { authConfig } from "./auth.config";
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { z } from "zod";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
-  ...authConfig,
+  pages: authConfig.pages,
+  callbacks: authConfig.callbacks,
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+
+          const admin = await prisma.admin.findUnique({
+            where: { email },
+          });
+
+          if (!admin) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, admin.password);
+          if (passwordsMatch) return admin;
+        }
+
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
 });
